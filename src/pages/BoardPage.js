dc17b70e5eb834a4b88cb8f61ec24799c499e535
dhanush788 <dhanushpk50@gmail.com>
2023-10-26 21:26:23 +0530

    changes

--- src/pages/BoardPage.js
+++ src/pages/BoardPage.js
@@ -35,23 +35,35 @@ export const BoardPage = withRouter(
         });
 
         const boardId = () => props.match?.params?.board;
+        console.log(board)
 
         const handleDataChange = async (data) => await boardService.updateBoard(boardId(), data);
 
         if (loading) {
             return <BoardSkeleton count={5} />;
         }
+        const handleAllocate = () => {
+            console.log(board)
+            // here
+        }
 
         return (
-            <Board
-                className={`pt-16 bg-blue-500 h-full`}
-                canAddLanes={true}
-                editable={true}
-                data={{
-                    lanes: board.lanes || [],
-                }}
-                onDataChange={handleDataChange}
-            />
+            <div className='bg-blue-500  h-full'>
+                <Board
+                    className={`pt-16 h-4/5 bg-blue-500 `}
+                    canAddLanes={true}
+                    editable={true}
+                    data={{
+                        lanes: board.lanes || [],
+                    }}
+                    onDataChange={handleDataChange}
+                />
+                <button 
+                onClick={handleAllocate}
+                className="glass-effect text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                    ALLOCATE
+                </button>
+            </div>
         );
     })
 );
