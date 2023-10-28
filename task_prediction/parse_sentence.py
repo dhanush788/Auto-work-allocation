8fef7913a63b7f223cf945cfa1092b5886a9d332
VergilX <abhinand.dmanoj@gmail.com>
2023-10-28 16:05:42 +0530

    Cleaned data and program for access

--- task_prediction/parse_sentence.py
+++ task_prediction/parse_sentence.py
@@ -3,35 +3,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import sys
 
-# Load the English language model
-nlp = spacy.load("en_core_web_sm")
-
-# Sample data for training the classifier
-data = []
-FILE = "data.txt" # Put file path here
-
-# Importing data from file
-with open(FILE, "r") as f:
-    for line in f:
-        if line[0] not in ["#", '\n']:
-            current = line.split(",")
-            current[1] = current[1][:-1]
-
-            data.append(tuple(current))
-
-# Tokenize and preprocess the text data
-texts, labels = zip(*data)
-tokenized_texts = [nlp(text) for text in texts]
-
-# Feature extraction using Bag of Words
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(texts)
-
-# Train a simple text classifier (Multinomial Naive Bayes)
-clf = MultinomialNB()
-clf.fit(X, labels)
-
-def determine_developer_type(task_description):
+def determine_developer_type(task_description, nlp, vectorizer, clf):
     # Tokenize and preprocess the task description
     task_doc = nlp(task_description)
     # Convert the text into a Bag of Words representation
@@ -40,6 +12,39 @@ def determine_developer_type(task_description):
     developer_type = clf.predict(task_vector)[0]
     return developer_type
 
-task_description = sys.argv[1]
-developer_type = determine_developer_type(task_description)
-print(f"Assign the task to a {developer_type}.")
+def parse(task_description):
+    # Load the English language model
+    nlp = spacy.load("en_core_web_sm")
+
+    # Sample data for training the classifier
+    data = []
+    FILE = "data.txt" # Put file path here
+
+    # Importing data from file
+    with open(FILE, "r") as f:
+        for line in f:
+            if line[0] not in ["#", '\n']:
+                current = line.split(",")
+                current[1] = current[1][:-1]
+
+                data.append(tuple(current))
+
+    # Tokenize and preprocess the text data
+    texts, labels = zip(*data)
+    tokenized_texts = [nlp(text) for text in texts]
+
+    # Feature extraction using Bag of Words
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(texts)
+
+    # Train a simple text classifier (Multinomial Naive Bayes)
+    clf = MultinomialNB()
+    clf.fit(X, labels)
+
+    developer_type = determine_developer_type(task_description, nlp, vectorizer, clf)
+
+    return developer_type
+
+if __name__ == "__main__":
+    task_description = sys.argv[1]
+    print(parse(task_description))
